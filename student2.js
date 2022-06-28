1138ca7d7ba4b73266ae2b920e3a7de124e34bc2
Zimmy <austinpzim@gmail.com>
2022-06-28 00:02:23 -0400

    Lab10 Update

---
+++ student2.js
@@ -0,0 +1,66 @@
+var companyname4 = "Book"; 
+var address4 = "269 Main St. Valencia, CA 91344";
+var phonenumber5 = "(555) 861-1234";
+var size = 3;
+
+//---------------------------------------------------------------------------
+
+var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
+var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
+var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
+
+//---------------------------------------------------------------------------
+
+function makeMenu4(myproduct){
+var msg = "<table style='margin-left:auto; margin-right:auto; border:3px solid gold; width:40%; background-color:gray; text-align:center; color:white; font-size:24px'><td>Menu:</td>";
+ msg += "<td  id = 'button1'><button onClick='myproduct = product1; myexecButton453(myproduct)'>Product1</button> </td>";
+ msg += "<td id = 'button2'><button onClick='myproduct = product2;execButton453(myproduct)'>Product2</button>  </td>";
+ msg += "<td id = 'button3'><button onClick='myproduct = product3;execButton453(myproduct)'>Product3</button>";
+ msg  += "</td></tr></table>";
+ return(msg);
+}
+
+function execButton453(myproduct){ 
+document.getElementById('button1').innerHTML = makeMain2(myproduct);
+document.getElementById('button2').innerHTML = makeMain2(myproduct);
+document.getElementById('button3').innerHTML = makeMain2(myproduct); 
+}
+
+
+
+function makeMain2(myproduct){
+this.myproduct = myproduct;
+var imgurl797 = "http://college1.com/images/" + myproduct.id + ".gif";
+ var data = "<p style='text-align:left; color:black'>";
+     data += "<h1>My Product:</h1>";
+     data += "Product Name: " + myproduct.name + "<br>"; 
+     data += "Product ID: " + myproduct.id + "<br>"; 
+     data += "Product Description: " + myproduct.desc + "<br>"; 
+     data += "Shopping Cart Link Bar <br>"; 
+     data += "Product Image: <img src="imgurl797" height=100 width=100></a>"; 
+     data += "</p>";
+     return(data); 
+}
+
+
+
+
+//----------------------------------------------------------------------------
+
+function getHeader(){
+  var now = new Date();
+  var data = "<table style='width:100%; text-align:center; color:gold; background-color:gray'>";
+  data += "<tr style='text-align:right'><td>" + now.toLocaleString() + "</td></tr>";
+  data +="<tr style='text-align:center'><td><h1>Zimmy's Books - Grand Opening Discounts!</h1>";
+  data +="</td></tr></table>";
+  return(data);
+}
+
+function getFooter(companyname4, address4, phonenumber5){
+     var data = "<table style='width:100%; background-color:gray; text-align:center; color:gold'><tr style='text-align:center'><td>";
+     data += navigator.appName  + "</td></tr>";
+     data += "<tr style='text-align:center'><td>" + navigator.appVersion  + "</td></tr>"; 
+     data += "<tr style='text-align:center'><td>" + navigator.platform; 
+     data += "</td></tr></table>";
+     return(data); 
+}
